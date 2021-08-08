438cf5e9e103db3525a740a43207fa621d04967b
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-08-08 17:40:29 -0400

    Update lrCostFunction.m

--- Week4/lrCostFunction.m
+++ Week4/lrCostFunction.m
@@ -27,25 +27,22 @@ grad = zeros(size(theta));
 %       prediction for that example. You can make use of this to vectorize
 %       the cost function and gradient computations. 
 %
-% Hint: When computing the gradient of the regularized cost function, 
-%       there're many possible vectorized solutions, but one solution
-%       looks like:
-%           grad = (unregularized gradient for logistic regression)
-%           temp = theta; 
-%           temp(1) = 0;   % because we don't add anything for j = 0  
-%           grad = grad + YOUR_CODE_HERE (using the temp variable)
-%
+
 
 h = sigmoid(X*theta);
 
+% The temp variable will hold all values of theta_j for j > 2
 temp = theta;
 temp(1) = 0;
 
+% Regularized Cost function for logistic regression
 J = 1/m * sum(-y.*log(h)-((-y+1).*log(-h+1))) + ...
     (lambda/(2*m) * sum(temp.^2));
 
+% Gradient function for logistic regression
 grad = 1/m * sum((h-y).*X);
 
+% Adjusts gradient function with the regularization term
 grad = grad + ((lambda/m).*temp)';
 
 
