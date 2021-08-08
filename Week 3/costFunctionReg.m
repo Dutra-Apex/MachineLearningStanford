c3e0a110527b28e44a4d24488f9c4bfcc83d6d9d
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-08-08 17:37:11 -0400

    Update costFunctionReg.m

--- Week 3/costFunctionReg.m
+++ Week 3/costFunctionReg.m
@@ -20,18 +20,15 @@ grad = zeros(size(theta));
 
 h = sigmoid(X*theta);
 
-theta_j = theta(2:end);
+temp = theta;
+temp(1) = 0;
 
 J = 1/m * sum(-y.*log(h)-((-y+1).*log(-h+1))) + ...
-    (lambda/(2*m) * sum(theta_j.^2));
+    (lambda/(2*m) * sum(temp.^2));
 
-%grad(1) = (1/m * sum((h(1)-y(1)).*X(1)));
+grad = 1/m * sum((h-y).*X);
 
-grad = (1 / m) * sum( X .* repmat((sigmoid(X*theta) - y), 1, size(X,2)) );
-grad(:,2:length(grad)) = grad(:,2:length(grad)) + (lambda/m)*theta(2:length(theta))';
-
-%grad(2:end) = ((1/m) * (sum((h(2: end)-y(2:end)).*X(2:end))) + ((lambda/m)*theta_j));
-
+grad = grad + ((lambda/m).*temp)';
 
 
 % =============================================================
